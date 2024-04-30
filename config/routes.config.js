8e563374d4bd1bd2185855b29c09cecb0d4ea6a2
David Honeyands <149825642+dhoneyands@users.noreply.github.com>
2024-04-30 21:29:04 +0200

    Add files via upload

---
+++ config/routes.config.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const posts = require("../controllers/posts.controller");
+const users = require("../controllers/users.controller");
+
+//import auth middleware
+const { checkAuth } = require('../middlewares/auth.middleware');
+
+/* users CRUD
+router.post("/users", users.create);
+router.get("/api/users", users.list);
+router.get("/users/:id", checkAuth, users.details);
+router.patch("/users/:id", users.update); // actualiza info parcial
+//router.put("/users/:id", users.update);  sustituye toda la info
+router.delete("/users/:id", users.delete);
+*/
+
+// posts CRUD
+router.post("/api/posts", checkAuth, posts.create);
+router.get("/api/posts", checkAuth, posts.list);
+router.get("/api/posts/:id", checkAuth, posts.details);
+router.patch("/api/posts/:id", checkAuth, posts.update);
+router.delete("/api/posts/:id", checkAuth, posts.delete);
+
+// login
+router.post("/api/users", users.create);
+router.post("/api/login", users.login);
+
+
+module.exports = router;
